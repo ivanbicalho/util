0d059becc6094d13b4bbd651ba98c414251537bf
Ivan <ivanribeirob@gmail.com>
2016-12-19 23:08:17 -0200

    Alterações class Mapper

--- Util/Mapper.cs
+++ Util/Mapper.cs
@@ -9,25 +9,66 @@ namespace Util
 {
     public static class Mapper
     {
-        public static TOut Map<TIn, TOut>(TIn objTIn) where TIn : class where TOut : class, new()
+        #region Properties
+
+        private const string EXCEPTION_DUPLICATE_NAMES = "Existem propriedades com o mesmo nome. Para utilizar o mapeamento com 'IgnoreCase', as propriedades devem ter nomes distintos";
+
+        #endregion
+
+        #region Public Methods
+
+        public static TOut Map<TIn, TOut>(TIn objTIn, bool ignoreCase = false)
+            where TIn : class
+            where TOut : class, new()
         {
             var objTOut = Activator.CreateInstance<TOut>();
-            return ExecuteMap(objTIn, objTOut);
+            return ExecuteMap(objTIn, objTOut, ignoreCase);
         }
 
-        public static TOut Map<TIn, TOut>(TIn objTIn, TOut objTOut) where TIn : class where TOut : class, new()
+        public static TOut Map<TIn, TOut>(TIn objTIn, TOut objTOut, bool ignoreCase = false)
+            where TIn : class
+            where TOut : class, new()
         {
-            return ExecuteMap<TIn, TOut>(objTIn, objTOut);
+            return ExecuteMap<TIn, TOut>(objTIn, objTOut, ignoreCase);
         }
 
-        private static TOut ExecuteMap<TIn, TOut>(TIn objTIn, TOut objTOut) where TIn : class where TOut : class, new()
+        public static IEnumerable<TOut> MapList<TIn, TOut>(IEnumerable<TIn> listTIn, bool ignoreCase = false)
+            where TIn : class
+            where TOut : class, new()
+        {
+            var listTOut = new List<TOut>();
+            return ExecuteMapList(listTIn, listTOut, ignoreCase);
+        }
+
+        public static IEnumerable<TOut> MapList<TIn, TOut>(IEnumerable<TIn> listTIn, List<TOut> listTOut, bool ignoreCase = false)
+            where TIn : class
+            where TOut : class, new()
+        {
+            return ExecuteMapList(listTIn, listTOut, ignoreCase);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static TOut ExecuteMap<TIn, TOut>(TIn objTIn, TOut objTOut, bool ignoreCase)
+            where TIn : class
+            where TOut : class, new()
         {
             if (objTIn == null) { return null; }
             if (objTOut == null) { objTOut = Activator.CreateInstance<TOut>(); }
 
+            if (ignoreCase) { ValidateCase(objTIn, objTOut); }
+
             foreach (var propTOut in objTOut.GetType().GetProperties())
             {
-                var propTIn = objTIn.GetType().GetProperty(propTOut.Name);
+                PropertyInfo propTIn;
+
+                if (ignoreCase)
+                    propTIn = objTIn.GetType().GetProperty(propTOut.Name, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                else
+                    propTIn = objTIn.GetType().GetProperty(propTOut.Name);
+
                 if (propTIn == null) { continue; }
 
                 if (propTIn.PropertyType == propTOut.PropertyType
@@ -56,5 +97,43 @@ namespace Util
 
             return objTOut;
         }
+
+        private static IEnumerable<TOut> ExecuteMapList<TIn, TOut>(IEnumerable<TIn> listTIn, List<TOut> listTOut, bool ignoreCase)
+            where TIn : class
+            where TOut : class, new()
+        {
+            if (listTIn == null) { return null; }
+            if (listTOut == null) { listTOut = new List<TOut>(); }
+
+            foreach (var objTIn in listTIn)
+            {
+                var objTOut = Activator.CreateInstance<TOut>();
+                listTOut.Add(ExecuteMap(objTIn, objTOut, ignoreCase));
+            }
+
+            return listTOut;
+        }
+
+        private static void ValidateCase<TIn, TOut>(TIn objTIn, TOut objTOut)
+            where TIn : class
+            where TOut : class, new()
+        {
+            var duplicateTInNames = objTIn.GetType().GetProperties().OfType<PropertyInfo>()
+                .GroupBy(item => item.Name.ToLower())
+                .Where(group => group.Count() > 1)
+                .Count();
+
+            var duplicateTOutNames = objTOut.GetType().GetProperties().OfType<PropertyInfo>()
+                .GroupBy(item => item.Name.ToLower())
+                .Where(group => group.Count() > 1)
+                .Count();
+
+            if (duplicateTInNames > 0 || duplicateTOutNames > 0)
+            {
+                throw new Exception(EXCEPTION_DUPLICATE_NAMES);
+            }
+        }
+
+        #endregion
     }
 }
